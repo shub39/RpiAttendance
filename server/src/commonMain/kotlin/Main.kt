b7baea28c2e8b07abbf3d849f1c08a7eefecffda
shub39 <cptnshubham39@gmail.com>
2025-12-13 12:46:39 +0530

    :zap: added deployment method
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- server/src/commonMain/kotlin/Main.kt
+++ server/src/commonMain/kotlin/Main.kt
@@ -1,15 +1,14 @@
 import io.ktor.client.*
 import io.ktor.client.engine.curl.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.runBlocking
 
 fun main() {
-    println("Hello World!")
 
-    val client = HttpClient(Curl)
-    val response = runBlocking {
-        client.get("https://ktor.io/")
+    val client = HttpClient(Curl) {
+        engine {
+            sslVerify = true
+            caInfo = "/etc/ssl/certs/ca-certificates.crt"
+            caPath = "/etc/ssl/certs"
+        }
     }
-    println(response.status)
-    client.close()
+
 }
