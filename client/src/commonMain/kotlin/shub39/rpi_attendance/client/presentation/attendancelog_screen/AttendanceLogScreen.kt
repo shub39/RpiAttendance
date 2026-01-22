f20e890e903b31df488dd741f7cc26e9a225a17f
shub39 <cptnshubham39@gmail.com>
2026-01-22 16:08:29 +0530

    :wrench: fixed icons
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- client/src/commonMain/kotlin/shub39/rpi_attendance/client/presentation/attendancelog_screen/AttendanceLogScreen.kt
+++ client/src/commonMain/kotlin/shub39/rpi_attendance/client/presentation/attendancelog_screen/AttendanceLogScreen.kt
@@ -1,17 +1,66 @@
 package shub39.rpi_attendance.client.presentation.attendancelog_screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.resources.painterResource
+import rpiattendance.client.generated.resources.Res
+import rpiattendance.client.generated.resources.arrow_forward_ios
 import shub39.rpi_attendance.client.presentation.theme.AppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AttendanceLogScreen(
     modifier: Modifier = Modifier,
+    padding: PaddingValues,
     state: AttendanceLogState,
     onAction: (AttendanceLogAction) -> Unit
 ) {
-
+    Scaffold(
+        modifier = modifier.padding(padding),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Attendance Logs")
+                }
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.arrow_forward_ios),
+                        contentDescription = null,
+                        tint = Color.Black
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview
@@ -20,7 +69,8 @@ private fun Preview() {
     AppTheme {
         AttendanceLogScreen(
             state = AttendanceLogState(),
-            onAction = {}
+            onAction = {},
+            padding = PaddingValues()
         )
     }
 }
--- client/src/commonMain/kotlin/shub39/rpi_attendance/client/presentation/attendancelog_screen/AttendanceLogState.kt
+++ client/src/commonMain/kotlin/shub39/rpi_attendance/client/presentation/attendancelog_screen/AttendanceLogState.kt
@@ -1,7 +1,16 @@
 package shub39.rpi_attendance.client.presentation.attendancelog_screen
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import models.Session
+import kotlin.time.Clock
 
+@Stable
+@Immutable
 data class AttendanceLogState(
-    val sessions: List<Session> = emptyList()
+    val sessions: List<Session> = emptyList(),
+    val selectedDate: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
 )
